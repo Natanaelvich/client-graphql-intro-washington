0320b40ef54aba1dd34338699d06d4788bdbc1ea
Natanael Lima <taelima1997@gmail.com>
2021-03-13 00:50:15 -0300

    create queries account

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -8,11 +8,42 @@ export const GET_CONTATOS = gql`
     }
   }
 `;
+export const GET_ACCOUNTS = gql`
+  query {
+    accounts {
+      _id
+      nome
+      email
+    }
+  }
+`;
 export const ADD_CONTATOS = gql`
   mutation createContato($nome: String!, $email: String!, $user_id: Int!) {
     createContato(data: { nome: $nome, email: $email, user_id: $user_id }) {
       nome
       email
+    }
+  }
+`;
+export const ADD_ACCOUNT = gql`
+  mutation createAccount($nome: String!, $email: String!) {
+    createAccount(data: { nome: $nome, email: $email }) {
+      nome
+      email
+    }
+  }
+`;
+export const DELETE_ACCOUNT = gql`
+  mutation deleteAccount($id: ID!) {
+    deleteAccount(id: $id)
+  }
+`;
+export const UPDATE_ACCOUNT = gql`
+  mutation updateAccount($id: ID!, $email: String!, $nome: String!) {
+    updateAccount(id: $id, data: { email: $email, nome: $nome }) {
+      nome
+      email
+      _id
     }
   }
 `;
