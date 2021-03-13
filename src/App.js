039ce2366b39037a7d4865a80ccb0ea2ea2248f6
Natanael Lima <taelima1997@gmail.com>
2021-03-13 00:49:47 -0300

    create crud acccount

--- src/App.js
+++ src/App.js
@@ -1,32 +1,52 @@
 import { useMutation, useQuery } from "@apollo/client";
 import { useState } from "react";
-import { ADD_CONTATOS, GET_CONTATOS } from "./graphql";
+import {
+  ADD_ACCOUNT,
+  GET_ACCOUNTS,
+  DELETE_ACCOUNT,
+  UPDATE_ACCOUNT,
+} from "./graphql";
+
+const cacheCreateAccount = {
+  update(cache, { data }) {
+    const newAccount = data?.createAccount;
+    const cacheAccounts = cache.readQuery({ query: GET_ACCOUNTS });
+
+    cache.writeQuery({
+      query: GET_ACCOUNTS,
+      data: {
+        accounts: [...cacheAccounts.accounts, newAccount],
+      },
+    });
+  },
+};
 
 function App() {
-  const { loading, data } = useQuery(GET_CONTATOS);
-  const [createContato] = useMutation(ADD_CONTATOS, {
-    update(cache, { data }) {
-      const newContato = data?.createContato;
-      const cacheContatos = cache.readQuery({ query: GET_CONTATOS });
-
-      cache.writeQuery({
-        query: GET_CONTATOS,
-        data: {
-          contatos: [...cacheContatos.contatos, newContato],
-        },
-      });
-    },
-  });
+  const { loading, data } = useQuery(GET_ACCOUNTS);
+  const [createAccount] = useMutation(ADD_ACCOUNT, cacheCreateAccount);
+  const [deleteAccount] = useMutation(DELETE_ACCOUNT);
+  const [updateAccount] = useMutation(UPDATE_ACCOUNT);
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
 
-  console.log(data);
-
   function handleSubmit(e) {
     e.preventDefault();
-    createContato({
-      variables: { nome: name, email, user_id: 52014318 },
+    createAccount({
+      variables: { nome: name, email },
+    });
+  }
+
+  function handleUpdate(id) {
+    updateAccount({
+      variables: { id, nome: name, email },
+    });
+  }
+
+  function handleDeleteAccount(id) {
+    deleteAccount({
+      variables: { id },
+      refetchQueries: [{ query: GET_ACCOUNTS }],
     });
   }
 
@@ -49,15 +69,39 @@ function App() {
         />
         <button type="submit">Cadastrar</button>
       </form>
+
       {loading ? (
         <p>Carregando</p>
       ) : (
         <ul>
-          {data.contatos.map((c, index) => (
-            <li key={index}>{c.email}</li>
+          {data.accounts.map((c) => (
+            <div key={`account-${c._id}`}>
+              <li>
+                {c._id} : {c.nome} : {c.email}
+                <button onClick={() => handleDeleteAccount(c._id)}>
+                  remove
+                </button>
+                <button onClick={() => handleUpdate(c._id)}>update</button>
+              </li>
+            </div>
           ))}
         </ul>
       )}
+
+      <label htmlFor="">Nome Atualizar</label>
+      <input
+        type="text"
+        name="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <label htmlFor="">Email Atualizar</label>
+      <input
+        type="text"
+        name="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
     </div>
   );
 }
